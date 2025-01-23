4b61bb72c9d1dd2675f1e8c9cc8e33557471c249
Egor Ivanov <rust_lang_lover@proton.me>
2025-01-23 23:59:39 +0300

    // checkpoint

--- application/core/schemas.py
+++ application/core/schemas.py
@@ -1,12 +1,46 @@
-from pydantic import BaseModel
+from typing import Optional
+
+from pydantic import BaseModel, field_validator, Field
+
+
+def luhn_check(imei: str) -> bool:
+    if len(imei) != 15 or not imei.isdigit():
+        return False
+    total = 0
+    for i, char in enumerate(imei):
+        digit = int(char)
+        if i % 2 == 1:
+            digit *= 2
+            if digit > 9:
+                digit = digit - 9
+        total += digit
+    return total % 10 == 0
+
+class AdditionalInfo(BaseModel):
+    deviceName: Optional[str] = None
+    image: Optional[str] = None
+    imei: Optional[str] = None
+    estPurchaseDate: Optional[int] = None
+    simLock: Optional[bool] = None
+    warrantyStatus: Optional[str] = None
+    repairCoverage: Optional[str] = None
+    technicalSupport: Optional[str] = None
+    modelDesc: Optional[str] = None
+    demoUnit: Optional[bool] = None
+    refurbished: Optional[bool] = None
+    purchaseCountry: Optional[str] = None
+    apple_region: Optional[str] = Field(None, alias="apple/region")  # Используем псевдоним для поля с "/"
+    fmiOn: Optional[bool] = None
+    lostMode: Optional[str] = None
+    usaBlockStatus: Optional[str] = None
+    network: Optional[str] = None
+    class Config:
+        extra = "allow"
 
 
 class CheckImeiResponse(BaseModel):
-    deviceName: str
-    imei: str
-    modelName: str
-    brand: str
-    manufacturer: str
+    is_succeeded: bool
+    additional_info: Optional[AdditionalInfo] = None | str
 
 
 class BasicResponse(BaseModel):
@@ -17,6 +51,14 @@ class BasicResponse(BaseModel):
 class CheckImeiBody(BaseModel):
     imei: str
     token: str
+
+    @field_validator("imei")
+    def validate_imei(cls, v):
+        if not v.isdigit() or len(v) != 15:
+            raise ValueError("IMEI must be a 15-digit number")
+        if not luhn_check(v):
+            raise ValueError("Invalid IMEI (failed Luhn check)")
+        return v
 
 
 class AddUserBody(BaseModel):
--- application/services/api.py
+++ application/services/api.py
@@ -1,20 +1,49 @@
 import os
+
+import aiohttp
 from sqlalchemy.ext.asyncio import AsyncSession
-from core.schemas import BasicResponse, AddUserBody
+from core.schemas import BasicResponse, AddUserBody, CheckImeiBody, CheckImeiResponse, AdditionalInfo
 from repositories.api import ApiRepository
 
 
 class ApiService:
     @staticmethod
-    async def check_user(db: AsyncSession, telegram_id: int) -> BasicResponse:
-        response = await ApiRepository.check(db, telegram_id)
-        return response
+    async def check_imei(application: CheckImeiBody) -> CheckImeiResponse:
+        try:
+            ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+            if ACCESS_TOKEN == application.token:
+                url = "https://api.imeicheck.net/v1/checks"
+                headers = {
+                    "Authorization": f"Bearer {os.getenv('THENEO_TOKEN')}",
+                    "Accept-Language": "en",
+                    "Content-Type": "application/json"
+                }
+                data = {
+                    "deviceId": f"{application.imei}",
+                    "serviceId": 22
+                }
+                async with aiohttp.ClientSession() as session:
+                    async with session.post(url, headers=headers, json=data) as response:
+                        response.raise_for_status()  # Проверка на ошибки HTTP
+                        result = await response.json()
+                        properties = result.get("properties", {})
+                        additional_info = AdditionalInfo(**properties)
+                        return CheckImeiResponse(
+                            is_succeeded=True,
+                            additional_info=additional_info
+                        )
+            else:
+                return CheckImeiResponse(is_succeeded=False, additional_info="Invalid access token")
+        except aiohttp.ClientError as e:
+            return CheckImeiResponse(is_succeeded=False, additional_info={"error": str(e)})
+        except Exception as e:
+            return CheckImeiResponse(is_succeeded=False, additional_info={"error": str(e)})
 
     @staticmethod
     async def add_user(db: AsyncSession, application: AddUserBody) -> BasicResponse:
         try:
-            TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-            if TELEGRAM_BOT_TOKEN == application.telegram_bot_token:
+            ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+            if ACCESS_TOKEN == application.token:
                 response = await ApiRepository.insert_user(db, application)
                 return response
             else:
@@ -23,3 +52,8 @@ class ApiService:
                 )
         except Exception as e:
             return BasicResponse(is_succeeded=False, additional_info=str(e))
+
+    @staticmethod
+    async def check_user(db: AsyncSession, telegram_id: int) -> BasicResponse:
+        response = await ApiRepository.check(db, telegram_id)
+        return response
