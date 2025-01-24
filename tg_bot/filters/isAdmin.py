8225e679c6ae3cc1e95fe3fcf3e760ad5c915c0e
Egor Ivanov <rust_lang_lover@proton.me>
2025-01-24 21:56:14 +0300

    // updated telegram bot

---
+++ tg_bot/filters/isAdmin.py
@@ -0,0 +1,37 @@
+from aiogram.filters import BaseFilter
+from aiogram.types import Message
+from pydantic import BaseModel
+import aiohttp
+
+# Модель для ответа API
+class BasicResponse(BaseModel):
+    is_succeeded: bool
+    additional_info: str
+
+# Фильтр для проверки, является ли пользователь администратором
+class IsAdmin(BaseFilter):
+    async def __call__(self, message: Message) -> bool:
+        extracted_id = message.from_user.id
+        url = f"http://application:8001/api/check-user?telegram_id={extracted_id}"
+
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as response:
+                    response.raise_for_status()  # Проверяем, что запрос успешен
+                    json_data = await response.json()  # Декодируем JSON-ответ
+
+                    # Преобразуем JSON в модель BasicResponse
+                    api_response = BasicResponse(**json_data)
+
+                    # Проверяем, является ли пользователь администратором
+                    if api_response.is_succeeded:
+                        return True  # Пользователь администратор
+                    else:
+                        return False  # Пользователь не администратор
+
+        except aiohttp.ClientError as e:
+            print(f"HTTP error occurred: {e}")
+            return False  # В случае ошибки считаем, что пользователь не администратор
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return False  # В случае ошибки считаем, что пользователь не администратор
--- tg_bot/main.py
+++ tg_bot/main.py
@@ -1,9 +1,21 @@
 import os
 from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
 from dotenv import load_dotenv
+
+from filters.isAdmin import IsAdmin
 
 load_dotenv("/.env")
 TOKEN = os.getenv("TOKEN")
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
+
+@dp.message(CommandStart() and IsAdmin())
+async def starting_info(message: Message):
+    await message.answer(text="Этот бот поможет получить информацию о IMEI.\nВведите в поле интересующий вас IMEI (последовательность из 15 цифр)")
+
+@dp.message(CommandStart() and not IsAdmin())
+async def starting_info(message: Message):
+    await message.answer(text="Этот бот поможет получить информацию о IMEI.\nВведите в поле интересующий вас IMEI (последовательность из 15 цифр)")
